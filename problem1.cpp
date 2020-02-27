5117f43ebab1799bd801e873c7090323db5059fb
quang phung <phungchiminhquang@gmail.com>
2020-02-27 12:46:10 +0700

    da them dong 2

--- problem1.cpp
+++ problem1.cpp
@@ -15,6 +15,7 @@ int main(int argv, char **argc) {
 	cout << "q[2]= " << q[2] << endl;
 	cout << "p[1][2] = " << p[1][2] << endl;
 	cout<< "da them dong 1"<<endl;
+	cout<<"da them dong 2"<<endl;
     //=============== Uncomment on submit ================//
     // if (argv == 2) printInPattern(atoi(argc[1]));
     //====================================================//
